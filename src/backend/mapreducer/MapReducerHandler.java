cbc01ba90ec6ca27e28ff79b83a1b25086622b74
Christy C. <p3210220@aueb.gr>
2024-04-10 20:45:01 +0300

    added map reducer handler

---
+++ src/backend/mapreducer/MapReducerHandler.java
@@ -0,0 +1,37 @@
+package src.backend.mapreducer;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import src.backend.utility.filterdata.FilterData;
+
+public class MapReducerHandler implements Runnable{
+    
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    private Socket requestSocket;
+    private MapReducer mapReducer;
+
+    public MapReducerHandler(Socket requestSocket, MapReducer mapReducer)
+    {
+        this.requestSocket = requestSocket;
+        this.mapReducer = mapReducer;
+    }
+
+    @Override
+    public void run()
+    {
+        try {
+            this.out = new ObjectOutputStream(requestSocket.getOutputStream());
+            this.in = new ObjectInputStream(requestSocket.getInputStream());
+            
+            FilterData filter_results = (FilterData) in.readObject();
+            mapReducer.Reduce(filter_results.getMapid(), filter_results.getFilters());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
