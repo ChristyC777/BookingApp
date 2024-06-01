c1bc8802bf367fbf10db24a4182cbf51dfc64802
Christy C. <p3210220@aueb.gr>
2024-06-01 19:01:03 +0300

    fixed Homapage Adapter and added png images

--- app/src/main/java/gr/aueb/ebookingapp/adapter/CollectionHomepageAdapter.java
+++ app/src/main/java/gr/aueb/ebookingapp/adapter/CollectionHomepageAdapter.java
@@ -8,28 +8,69 @@ import android.widget.BaseAdapter;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CollectionHomepageAdapter extends BaseAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import gr.aueb.ebookingapp.R;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.List;
+
+import gr.aueb.ebookingapp.domain.backend.lodging.Lodging;
+
+public class CollectionHomepageAdapter extends BaseAdapter {
+    private Context context;
+    private List<Lodging> rooms;
     private LayoutInflater inflater;
 
+    public CollectionHomepageAdapter(Context context, List<Lodging> rooms) {
+        this.context = context;
+        this.rooms = rooms;
+        this.inflater = LayoutInflater.from(context);
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return rooms.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return rooms.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.viewrooms, parent, false);
+        }
+
+        ImageView lodgeImage = convertView.findViewById(R.id.lodgeImage);
+        TextView lodgeName = convertView.findViewById(R.id.lodgeName);
+        TextView lodgeStar = convertView.findViewById(R.id.lodgeStar);
+        TextView lodgePrice = convertView.findViewById(R.id.lodgePrice);
+        TextView textPrice = convertView.findViewById(R.id.textPrice);
+
+        Lodging room = rooms.get(position);
+
+        // Set the data
+        lodgeName.setText(room.getRoomName());
+        lodgeStar.setText(room.getStars());
+        lodgePrice.setText(String.valueOf(room.getPrice()));
+
+        // Assuming you have a method to get the drawable id from room
+        int imageResId = context.getResources().getIdentifier(room.getRoomImage(), "drawable", context.getPackageName());
+        lodgeImage.setImageResource(imageResId);
+
+        return convertView;
     }
 }
+
